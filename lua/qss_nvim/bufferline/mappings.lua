53d3362f1a3b6298e05b0ebcdd8392616ba9f4b4
Maxime <maximemikotajewski@gmail.com>
2023-04-24 18:09:15 +0200

    Added mappings for bufferline

--- lua/qss_nvim/bufferline/mappings.lua
+++ lua/qss_nvim/bufferline/mappings.lua
@@ -1,6 +1,8 @@
 M = { n = {
 	["<TAB>"] = { "<cmd> BufferLineCycleNext <CR>", "Go to next tab"},
-	["<S-TAB>"] = { "<cmd> BufferLineCyclePrev <CR>", "Go to next tab"},
+	["<S-TAB>"] = { "<cmd> BufferLineCyclePrev <CR>", "Go to previous tab"},
+	["<leader>ct"] = { "<cmd> BufferLinePickClose <CR>", "Close chosen tab"},
+	["<leader>ot"] = { "<cmd> BufferLinePick <CR>", "Open chosen tab"},
 }
 }
 
