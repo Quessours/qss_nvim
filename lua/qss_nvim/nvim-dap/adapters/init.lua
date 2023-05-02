6a20c95330c5f93a38b9cf1a5275b8a639fc028a
Maxime <maximemikotajewski@gmail.com>
2023-05-02 18:57:49 +0200

    Made C++ debugging work

--- lua/qss_nvim/nvim-dap/adapters/init.lua
+++ lua/qss_nvim/nvim-dap/adapters/init.lua
@@ -1,2 +1,4 @@
 require("qss_nvim.nvim-dap.adapters.lua")
 require("qss_nvim.nvim-dap.adapters.python")
+--require("qss_nvim.nvim-dap.adapters.cpp_gdb")
+require("qss_nvim.nvim-dap.adapters.cpp_lldb")
--- lua/qss_nvim/nvim-dap/adapters/lua.lua
+++ lua/qss_nvim/nvim-dap/adapters/lua.lua
@@ -1,5 +1,5 @@
-local dap = require('dap')
-
+dap = require('dap')
+--[[
 dap.configurations.lua = {
 	{
 		type = "nlua",
@@ -22,4 +22,17 @@ dap.configurations.lua = {
 
 dap.adapters.nlua = function(callback, config)
 	callback { type = "server", host = config.host, port = config.port, log = true }
+end
+--]]
+local dap = require("dap")
+dap.configurations.lua = {
+	{
+		type = 'nlua',
+		request = 'attach',
+		name = "Attach to running Neovim instance",
+	}
+}
+
+dap.adapters.nlua = function(callback, config)
+	callback({ type = 'server', host = config.host or "127.0.0.1", port = config.port or 8086 })
 end
