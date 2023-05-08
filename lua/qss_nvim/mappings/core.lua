a4bad55cdec5aed957b1f40f0ced1e5f341bb3a2
Maxime <maximemikotajewski@gmail.com>
2023-05-08 19:21:48 +0200

    Added fancy mappings for vertical movement

--- lua/qss_nvim/mappings/core.lua
+++ lua/qss_nvim/mappings/core.lua
@@ -1,6 +1,11 @@
 M = {
     n = {
         ["<leader>af"] = { vim.lsp.buf.format, "focus nvimtree" },
+        ["gb"] = { "<C-O>", "Go back" },
+        ["<C-d>"] = { "<C-d>zz" },
+        ["<C-u>"] = { "<C-u>zz" },
+        ["n"] = { "nzzzv" },
+        ["N"] = { "Nzzzv" }
     }
 }
 
