ce845f7ae1e439f4afbfeb224fb458347c4d5fb0
Maxime Mikotajewski <mmikotajewski@vivlio.com>
2025-10-08 13:43:13 +0200

    added uml management

--- init.lua
+++ init.lua
@@ -2,7 +2,6 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 vim.diagnostic.config({ virtual_text = true })
-vim.diagnostic.config({ virtual_text = true })
 
 local lsp = require("qss_nvim.lsp_init")
 
