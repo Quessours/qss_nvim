cb14b622e75f03deb91e081f96ea58b965545c24
Maxime <maximemikotajewski@gmail.com>
2025-12-24 11:29:34 +0100

    Cleaned a bit

--- lua/plugins/rust-tools.lua
+++ lua/plugins/rust-tools.lua
@@ -1,30 +1,4 @@
 local opts = require("qss_nvim.rust-tools.config")
 local apply_mappings = require("qss_nvim.utils").apply_mappings
 
-local M = {
-    url = "git@github.com:Quessou/rust-tools.nvim.git",
-    branch = "feature/improved_stuff",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "mfussenegger/nvim-dap",
-    },
-    cond = function()
-        local dir_content = require("qss_nvim.utils").scan_dir()
-
-        for _, f in pairs(dir_content) do
-            if f == "Cargo.toml" then
-                return true
-            end
-        end
-        return false
-    end,
-    config = function()
-        require("rust-tools").setup(opts)
-        local mappings = require("qss_nvim.rust-tools.mappings")
-        assert(mappings ~= nil)
-        apply_mappings(mappings)
-    end,
-}
-
---return M
 return {}
--- lua/plugins/rustacean-nvim.lua
+++ lua/plugins/rustacean-nvim.lua
@@ -5,6 +5,16 @@ return {
     'mrcjkb/rustaceanvim',
     version = '^6', -- Recommended
     lazy = false,   -- This plugin is already lazy
+    cond = function()
+        local dir_content = require("qss_nvim.utils").scan_dir()
+
+        for _, f in pairs(dir_content) do
+            if f == "Cargo.toml" then
+                return true
+            end
+        end
+        return false
+    end,
     config = function()
         require("rustaceanvim")
         vim.g.rustaceanvim = opts
